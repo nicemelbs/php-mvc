bcddf3f8076a1fee9003220b438805a066438407
Melvin San Jose <sanjose.melvin@gmail.com>
2022-04-17 22:29:08 +0800

    Remove commented HTML

--- views/register.php
+++ views/register.php
@@ -20,26 +20,4 @@ use app\core\forms\Form;
 <?php echo $form->field($model, 'password')->setType(Field::TYPE_PASSWORD); ?>
 <?php echo $form->field($model, 'passwordConfirm')->setType(Field::TYPE_PASSWORD); ?>
 <button type="submit" class="btn btn-primary">Submit</button>
-<!--<div class="row mb-3">-->
-<!--    <div class="col">-->
-<!--        <label for="firstname" class="form-label">First Name</label>-->
-<!--        <input type="text" class="form-control" id="firstname" name="firstname">-->
-<!--    </div>-->
-<!--    <div class="col">-->
-<!--        <label for="lastname" class="form-label">Last Name</label>-->
-<!--        <input type="text" class="form-control" id="lastname" name="lastname">-->
-<!--    </div>-->
-<!--</div>-->
-<!--<div class="mb-3">-->
-<!--    <label for="email" class="form-label">Email</label>-->
-<!--    <input type="email" class="form-control" id="email" name="email">-->
-<!--</div>-->
-<!--<div class="mb-3">-->
-<!--    <label for="password" class="form-label">Password</label>-->
-<!--    <input type="password" class="form-control" name="password" id="password">-->
-<!--    <label for="passwordConfirm" class="form-label">Confirm Password</label>-->
-<!--    <input type="password" class="form-control" name="passwordConfirm" id="passwordConfirm">-->
-<!--</div>-->
-<!--<button type="submit" class="btn btn-primary">Submit</button>-->
 <?php echo Form::end(); ?>
-<!--</form>-->
