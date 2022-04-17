c17093fa9de706032914c284a7d4861e0d938788
Melvin San Jose <sanjose.melvin@gmail.com>
2022-04-17 14:24:24 +0800

    Working on validations

--- views/register.php
+++ views/register.php
@@ -2,12 +2,12 @@
 <form action="" method="post">
     <div class="row mb-3">
         <div class="col">
-            <label for="first-name" class="form-label">First Name</label>
-            <input type="text" class="form-control" id="first-name" name="first-name" aria-describedby="emailHelp">
+            <label for="firstname" class="form-label">First Name</label>
+            <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="emailHelp">
         </div>
         <div class="col">
-            <label for="last-name" class="form-label">Last Name</label>
-            <input type="text" class="form-control" id="last-name" name="last-name" aria-describedby="emailHelp">
+            <label for="lastname" class="form-label">Last Name</label>
+            <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="emailHelp">
         </div>
     </div>
     <div class="mb-3">
@@ -17,8 +17,8 @@
     <div class="mb-3">
         <label for="password" class="form-label">Password</label>
         <input type="password" class="form-control" name="password" id="password">
-        <label for="confirm-password" class="form-label">Confirm Password</label>
-        <input type="password" class="form-control" name="confirm-password" id="confirm-password">
+        <label for="passwordConfirm" class="form-label">Confirm Password</label>
+        <input type="password" class="form-control" name="passwordConfirm" id="passwordConfirm">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
